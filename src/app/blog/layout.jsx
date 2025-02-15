e4009149c040707c859920237f138f8566ef2dd4
LokeshMohanMahankali <lokeshmahankali@gmail.com>
2025-02-15 19:29:30 +0530

    changes

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogLayoouts = ({ children }) => {
+  return (
+    <div>
+      <h1>Hi this is blog</h1>
+      {children}
+    </div>
+  );
+};
+
+export default BlogLayoouts;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        asfdasfd
-        {children}
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
